a9a940bdc16e667f42362c8ad7570e2f21be4039
Ichigo27 <amit97ramani@gmail.com>
2022-10-27 14:23:48 +0530

    watch, register and login pages created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./app.scss";
-import Home from "./home/Home.jsx";
+import Home from "./pages/home/Home.jsx";
+import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
 
 const App = () => {
-  return <Home/>;
+    return <Home />;
 };
 
 export default App;
